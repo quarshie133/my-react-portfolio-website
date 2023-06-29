7d2ec65d32549ed564e6100052c631025bbae258
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-06-29 22:43:36 +0000

    experience layout styling

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,72 @@
 import React from "react";
 import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-  <section id="experience">Experience</section>
-  
+    <section id="experience">
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Fontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Expereinced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>React Js</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>C#</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>ASP.NET Framework MVC</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONTEND */}
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Node JS</h4>
+              <small className="text-light">Beginner</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
