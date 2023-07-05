8666fb73c6b4b9ad137d3cb8c9b1a8bb64181349
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-07-05 23:28:38 +0000

    services component styling

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import './services.css';
-
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <section id='services'>
-       services
-    </section>
-  )
-}
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services</h2>
 
-export default Services
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Web development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Websites</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Web Applications</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>CMS</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Maintenance</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
