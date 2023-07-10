dc7b016b6fd7540202d1f0d2128ad47276b22bf7
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-07-10 13:34:57 +0000

    mapping through project images using the map method and destructuring

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio1.png";
-import IMG2 from "../../assets/portfolio2.png";
-import IMG3 from "../../assets/portfolio3.png";
+import { data } from "../../data";
 
 const Portfolio = () => {
   return (
@@ -10,28 +8,24 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" className="portfolio-image" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/quarshie133/Netflix-Clone"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://react-netflix-clone-9f7bfd.netlify.app/login"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} className="portfolio-image" />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -1,23 +1,27 @@
-const data = [
+import IMG1 from "../src/assets/portfolio1.png";
+import IMG2 from "../src/assets/portfolio2.png";
+import IMG3 from "../src/assets/portfolio3.png";
+
+export const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Netflix",
-    github: "https://github.com/quarshie133/Netflix-Clone",
-    demo: "https://react-netflix-clone-9f7bfd.netlify.app/login",
+    title: "Cocktails Web App",
+    github: "https://github.com/quarshie133/cocktails-webapp",
+    demo: "https://react-project-cocktails-webapp.netlify.app/",
   },
   {
     id: 2,
     image: IMG2,
-    title: "Kwame Gyan Minitries",
+    title: "Kwame Gyan Ministries",
     github: "https://github.com/quarshie133/kwame-gyan-ministries",
     demo: "https://kwamegyan-ministries-b30716.netlify.app/",
   },
   {
     id: 3,
     image: IMG3,
-    title: "Cocktails Web App",
-    github: "https://github.com/quarshie133/cocktails-webapp",
-    demo: "https://react-project-cocktails-webapp.netlify.app/",
+    title: "Netflix",
+    github: "https://github.com/quarshie133/Netflix-Clone",
+    demo: "https://react-netflix-clone-9f7bfd.netlify.app/login",
   },
 ];
