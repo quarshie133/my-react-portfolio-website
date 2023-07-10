2abb8e46bfd0d5f190c93b6795e6995dccf6a4a1
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-07-10 09:58:34 +0000

    portfolio layout section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.png";
+import IMG2 from "../../assets/portfolio2.png";
+import IMG3 from "../../assets/portfolio3.png";
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>port</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a
+            href="https://github.com/quarshie133/Netflix-Clone"
+            className="btn"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            href="https://react-netflix-clone-9f7bfd.netlify.app/login"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
