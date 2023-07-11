a5d12959ac5dadaca38a99dd25c8625381629c9a
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-07-11 16:42:33 +0000

    Contact layout styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className=".contact__option-icon " />
             <h4>Email</h4>
             <h5>wrightsprince@gmail.com</h5>
             <a href="mailto:wrightsprince@gmail.com" target="_blank">
@@ -20,7 +20,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className=".contact__option-icon " />
             <h4>Messenger</h4>
             <h5>Prince Wrights Quarshie</h5>
             <a href="https://m.me/prince.wrightsquarshie" target="_blank">
@@ -28,7 +28,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className=".contact__option-icon " />
             <h4>WhatsApp</h4>
             <a
               href="https://api.whatsapp.com/send?phone+233272515799"
@@ -40,15 +40,28 @@ const Contact = () => {
         </div>
         {/* END OF CONTACT OPTIONS */}
         <form action="">
-          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
 
           <input type="email" placeholder="Your Email" required />
-          <textarea name="message" id="" rows="7" placeholder="Your Message" required></textarea>
+          <textarea
+            name="message"
+            id=""
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
-        <button type="submit" className="btn btn-primary">Send Message</button>
       </div>
     </section>
   );
-}; 
+};
 
 export default Contact;
