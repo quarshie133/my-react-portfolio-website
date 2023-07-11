1803377ea674db61c6476d5231a8c24b81881fcd
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-07-11 11:49:47 +0000

    testimonial custom data added in datajs called reviews, this are reveiws or feedbacks from some clients I worked with

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./testimonials.css";
-import AVTR1 from "../../assets/avatar1.jpg";
-import AVTR2 from "../../assets/avatar2.jpg";
-import AVTR3 from "../../assets/avatar3.jpg";
+
+import { reviews } from "../../data";
 
 const Testimonials = () => {
   return (
@@ -11,41 +10,9 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avartar One" />
-          </div>
-          <h5 className="client__name">Mark Oppong</h5>
-          <small className="cleint__review">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
-            corrupti, expedita nisi sapiente perferendis vero? Dicta nulla nemo
-            consequatur cum sunt sequi cumque aspernatur quae? Eligendi
-            dignissimos laudantium soluta. Adipisci?
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avartar One" />
-          </div>
-          <h5 className="client__name">Mark Oppong</h5>
-          <small className="cleint__review">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
-            corrupti, expedita nisi sapiente perferendis vero? Dicta nulla nemo
-            consequatur cum sunt sequi cumque aspernatur quae? Eligendi
-            dignissimos laudantium soluta. Adipisci?
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avartar One" />
-          </div>
-          <h5 className="client__name">Mark Oppong</h5>
-          <small className="cleint__review">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
-            corrupti, expedita nisi sapiente perferendis vero? Dicta nulla nemo
-            consequatur cum sunt sequi cumque aspernatur quae? Eligendi
-            dignissimos laudantium soluta. Adipisci?
-          </small>
+        {
+          reviews.map()
+        }
         </article>
       </div>
     </section>
--- src/data.js
+++ src/data.js
@@ -3,6 +3,11 @@ import IMG2 from "../src/assets/portfolio2.png";
 import IMG3 from "../src/assets/portfolio3.png";
 import IMG4 from "../src/assets/portfolio4.png";
 
+import Avt1 from "../src/assets/avatar1.jpg";
+import Avt2 from "../src/assets/avatar2.jpg";
+import Avt3 from "../src/assets/avatar3.jpg";
+import Avt4 from "../src/assets/avatar4.jpg";
+
 export const data = [
   {
     id: 1,
@@ -31,5 +36,29 @@ export const data = [
     title: "Upward Bound Agency",
     github: "",
     demo: "https://upwardboundagency.com/",
+  },
+];
+
+export const reveiws = [
+  {
+    avatar: Avt1,
+    name: "Mark Oppong",
+    reveiw: "Working with you has been an absolute pleasure, Thank You",
+  },
+  {
+    avatar: Avt2,
+    name: "Silas",
+    reveiw: "Great experience working with you.",
+  },
+  {
+    avatar: Avt3,
+    name: "Henry",
+    reveiw:
+      "A wonderful experience was all the help I got from the team. Thanks.",
+  },
+  {
+    avatar: Avt4,
+    name: "Frank",
+    reveiw: "looking forward to working with you again.",
   },
 ];
