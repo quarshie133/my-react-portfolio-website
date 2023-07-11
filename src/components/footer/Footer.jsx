4c0998c251ace0175bfc25501e943686212cf5da
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-07-11 22:20:28 +0000

    footer layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Prince Ephraim Quarshie
+      </a>
+
+      <ul className="permalink">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://www.linkedin.com/in/prince-ephraim-quarshie/">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/quarshie133">
+          <BsGithub />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
